887ac6f92ba12a84259ac30d62bedb5cc4e4ac08
Nilesh-Patil0310 <patilnilesh093@gmail.com>
2024-04-17 16:46:26 +0530

    implement project functionality

--- src/features/product/product.repository.js
+++ src/features/product/product.repository.js
@@ -56,7 +56,7 @@ class ProductRepository {
             filterExpression={$and:[{category:{$in: categories}}, filterExpression]}
             // filterExpression.category=category
         }
-        return collection.find(filterExpression).toArray();
+        return collection.find(filterExpression).project({name:1,price:1,_id:0,ratings:{$slice:1}}).toArray();
 
     }catch(err){
         console.log(err);

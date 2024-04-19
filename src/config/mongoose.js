879c1fd1c9c9ddc0077e70e40139e49b01411623
Nilesh-Patil0310 <patilnilesh093@gmail.com>
2024-04-19 15:44:29 +0530

    changes in user functinality using mongoose

---
+++ src/config/mongoose.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const url = process.env.DB_URL;
+
+export const connectUsingMongoose = async()=>{
+    
+    try{
+        await mongoose.connect(url,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log('MongoDB connected using mongoose');
+    }catch(err){
+        console.log('Error while connecting to db');
+        console.log(err);
+    }
+}
